60116dc44e2460dead02d3b0e24848202af3e2b4
Manuel Blanquett <manuel.blanquett@engelbert-strauss.de>
2019-01-08 12:08:07 +0100

    redeploy-script: Use oneliner

--- redeploy.sh
+++ redeploy.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-docker-compose build deathstar-canteen
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --no-deps -d deathstar-canteen
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --no-deps --build -d deathstar-canteen
