4c4df460579ecf9ea72ef63c8077e0c2ad6f7139
Manuel Blanquett <manuel.blanquett@engelbert-strauss.de>
2019-01-08 12:04:48 +0100

    docker-compose: Also run unit tests

--- src/Deathstar.Canteen/Program.cs
+++ src/Deathstar.Canteen/Program.cs
@@ -1,25 +1,15 @@
-using System.IO;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Serilog;
 
 namespace Deathstar.Canteen
 {
 	internal static class Program
 	{
-		private static IWebHostBuilder CreateWebHostBuilder(string[] args)
-		{
-			IConfigurationRoot hostConfig = new ConfigurationBuilder()
-				.SetBasePath(Directory.GetCurrentDirectory())
-				.AddJsonFile("hosting.json", true)
-				.Build();
-
-			return WebHost.CreateDefaultBuilder(args)
-				.UseConfiguration(hostConfig)
+		private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
 				.UseStartup<Startup>()
 				.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration));
-		}
 
 		private static void Main(string[] args) => CreateWebHostBuilder(args).Build().Run();
 	}
